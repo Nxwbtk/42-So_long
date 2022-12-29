b94d495eb4cca01c6d4710553775d03640c9b4a2
Bunthakan Sirikamonthip <bsirikam@z3t1c4.42bangkok.com>
2022-12-29 22:42:55 +0700

    add dern

--- ft_dern.c
+++ ft_dern.c
@@ -1,6 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_dern.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsirikam <bsirikam@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 22:41:45 by bsirikam          #+#    #+#             */
+/*   Updated: 2022/12/29 22:42:36 by bsirikam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
-void	ft_dern(t_map *real_map, int r, int c)
+void	ft_dern(t_map *m, int r, int c)
 {
-	
+	if (m->map[m->p_pos_r][m->p_pos_c] == 'E')
+	{
+		mlx_put_image_to_window(m->mlx, m->win, m->floor, m->p_pos_r * 64, \
+		m->p_pos_c * 64);
+	}
 }
