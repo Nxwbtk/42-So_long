595473a9f6f8167c7bf7bda92a1164f03d5746b6
Bunthakan Sirikamonthip <bsirikam@z3t1c4.42bangkok.com>
2022-12-29 18:20:27 +0700

    fix ja

--- ft_key_hook.c
+++ ft_key_hook.c
@@ -6,13 +6,13 @@
 /*   By: bsirikam <bsirikam@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 16:54:27 by bsirikam          #+#    #+#             */
-/*   Updated: 2022/12/29 17:28:45 by bsirikam         ###   ########.fr       */
+/*   Updated: 2022/12/29 18:15:44 by bsirikam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_check_walk(t_map *real_map, int r, int c, int key_code);
+void	ft_check_walk(t_map *real_map, int *r, int *c, int key_code);
 void	test_walk(t_map *real_map, int r, int c);
 
 void	ft_come_hook(t_map *real_map)
@@ -24,18 +24,20 @@ void	ft_come_hook(t_map *real_map)
 
 int	ft_key_hook(int key_code, t_map *real_map)
 {
-	int	r;
-	int	c;
+	static int	r = -1;
+	static int	c = -1;
 
-	r = real_map->p_pos_r;
-	c = real_map->p_pos_c;
-	// ft_printf("Test ja\n");
-	ft_check_walk(real_map, r, c, key_code);
+	if (r == -1)
+	{
+		r = real_map->p_pos_r;
+		c = real_map->p_pos_c;		
+	}
+	ft_check_walk(real_map, &r, &c, key_code);
 	ft_printf("Key code = %d\n", key_code);
 	return (0);
 }
 
-void	ft_check_walk(t_map *real_map, int r, int c, int key_code)
+void	ft_check_walk(t_map *real_map, int *r, int *c, int key_code)
 {
 	if (key_code == 53)
 	{
@@ -45,25 +47,27 @@ void	ft_check_walk(t_map *real_map, int r, int c, int key_code)
 	}
 	if (key_code == 0 || key_code == 123)
 	{
-		c--;
+		(*c)--;
 		// ft_printf("Test\n");
-		test_walk(real_map, r, c);
-		if (real_map->map[r][c] == '0' || real_map->map[r][c] == 'C' || real_map->map[r][c] == 'E')
+		test_walk(real_map, *r, *c);
+		if (real_map->map[*r][*c] == '0' || real_map->map[*r][*c] == 'C' || real_map->map[*r][*c] == 'E')
 		{
-			mlx_put_image_to_window(real_map->mlx, real_map->win, real_map->floor, c * 64, r * 64);
+			mlx_put_image_to_window(real_map->mlx, real_map->win, real_map->floor, *c * 64, *r * 64);
 		}
 	}
 	if (key_code == 13 || key_code == 126)
 	{
-		ft_printf("r = %d\t", r);
-		r = r - 1;
-		ft_printf("r = %d\t", r);
-		test_walk(real_map, r, c);
-		if (real_map->map[r][c] == '0' || real_map->map[r][c] == 'C' || real_map->map[r][c] == 'E')
+		ft_printf("r = %d\t", *r);
+		ft_printf("r = %d\t", *r);
+		(*r)--;
+		ft_printf("r = %d\t", *r);
+		ft_printf("r = %d\t", *r);
+		test_walk(real_map, *r, *c);
+		if (real_map->map[*r][*c] == '0' || real_map->map[*r][*c] == 'C' || real_map->map[*r][*c] == 'E')
 		{
-			mlx_put_image_to_window(real_map->mlx, real_map->win, real_map->floor, c * 64, r * 64);
-			mlx_put_image_to_window(real_map->mlx, real_map->win, real_map->player, c * 64, r * 64);
-			mlx_put_image_to_window(real_map->mlx, real_map->win, real_map->floor, c * 64, (r + 1) * 64);
+			mlx_put_image_to_window(real_map->mlx, real_map->win, real_map->floor, *c * 64, *r * 64);
+			mlx_put_image_to_window(real_map->mlx, real_map->win, real_map->player, *c * 64, *r * 64);
+			mlx_put_image_to_window(real_map->mlx, real_map->win, real_map->floor, *c * 64, (*r + 1) * 64);
 		}
 	}
 	// if (key_code == 2 || key_code == 124)
